bb137c13634434d3719e0fea6f9b066bd9ee1b0b
Jesse Averbukh <javerbukh@gmail.com>
2016-07-25 15:38:10 -0400

    Added docstrings and organized functions

--- JWST_pipeline_test.py
+++ JWST_pipeline_test.py
@@ -1,9 +1,26 @@
+"""
+This module contains the front end interface for the JWST Calibration
+Pipeline validation
+"""
+
 import sys
 from PIL import Image
 import glob
 from astropy.io import fits
 import pytest
 import argparse
+
+def setup_parser(parser):
+    """
+    Establishes the arguments this application will look for, including -h for
+    help and the input_file and expected_output_file
+    """
+    parser.add_argument("input_file", help="the file to be put through pipeline testing")
+    parser.add_argument("expected_output_file", help="once the input file goes through the pipeline, it will be compared to this file for validation")
+    parser.add_argument("-v", "--verbosity", action="count", default=0)
+
+    args = parser.parse_args()
+    return args
 
 def get_file_names(args):
     """
@@ -16,16 +33,14 @@ def get_file_names(args):
     return (input_file, expected_output_file)
 
 def start_tests(input_file):
+    """
+    Calls pytest in order to begin testing the validity of the pipeline using
+    the input_file and expected_output_file
+    """
     pytest.main(['-v', '--fname='+input_file])
 
 parser = argparse.ArgumentParser()
-parser.add_argument("input_file", help="the file to be put through pipeline testing")
-parser.add_argument("expected_output_file", help="once the input file goes through the pipeline, it will be compared to this file for validation")
-parser.add_argument("-v", "--verbosity", action="count", default=0)
-
-args = parser.parse_args()
-print (args.verbosity)
+args = setup_parser(parser)
 (input_file, expected_output_file) = get_file_names(args)
 start_tests(input_file)
-#start_tests(input_file, expected_output_file)
 print (input_file, expected_output_file)
